a9bc17e76db4e9ebd364fd0747d438ab061ecf50
manasav1212 <manasav@unm.edu>
2025-12-30 22:06:33 -0700

    Number Guessing Game Project

--- Number Guessing Game/src.js
+++ Number Guessing Game/src.js
@@ -1,11 +1,42 @@
+const readline = require("readline");
+
+const rd = readline.createInterface({input: process.stdin, output: process.stdout});
+
 function generateRandomNumber()
 {
-
+    let number = Math.floor(Math.random() * 100) + 1;
+    return number;
 }
 
-function isGuessCorrect()
+function isGuessCorrect(num, guess)
 {
-
+    if(Number.isNaN(guess))
+        return "Not a number";
+    if(num === guess)
+        return "Correct!";
+    else if (num > guess)
+        return "Too low";
+    else
+        return "Too high";
 }
 
-const num = generateRandomNumber();
+function keepGuessing()
+{
+    let result = "";
+    rd.question("Guess the number", 
+        (input) => {
+            const guess = Number(input.trim());
+            result = isGuessCorrect(num, guess);
+            if(result !== "Correct!")
+            {
+                console.log(result);
+                keepGuessing();
+            }
+            else
+                rd.close();
+        });
+        
+}
+
+const num = Number(generateRandomNumber());
+keepGuessing();
